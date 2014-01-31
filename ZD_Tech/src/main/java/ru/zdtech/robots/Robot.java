4fe51e4c3655a4c8f57f04bff31cc842e42b03b3
QIvan <QtIvan@gmail.com>
2014-01-31 22:23:24 +0400

    added simple solve for robot witch can go just up, down, left, right

--- ZD_Tech/src/main/java/ru/zdtech/robots/Robot.java
+++ ZD_Tech/src/main/java/ru/zdtech/robots/Robot.java
@@ -1,6 +1,6 @@
 package ru.zdtech.robots;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,18 +10,75 @@ import java.util.List;
 class Robot
 {
     private Field field;
+    private Position currentPos;
+
+    private List<Position> solution = new LinkedList<Position>();
 
     public Robot(Field field)
     {
         this.field = field;
+        currentPos = field.getStart();
+        solution.add(field.getStart());
     }
 
+    public void step(Position position)
+    {
+        currentPos = position;
+        solution.add(position);
+    }
+
+    private enum Step
+    {
+        UP(0, -1), RIGHT(1, 0), DOWN(0, 1), LEFT(-1, 0);
+        private int deltax;
+        private int deltay;
+
+        Step(int deltax, int deltay)
+        {
+            this.deltax = deltax;
+            this.deltay = deltay;
+        }
+        public Position getStep(Position pos)
+        {
+            return new Position(pos.getX() + deltax, pos.getY() + deltay);
+        }
+    }
+
+    public List<Position> getAllSteps()
+    {
+        List<Position> steps = new LinkedList<Position>();
+        for (Step step : Step.values())
+        {
+            steps.add(step.getStep(currentPos));
+        }
+        return field.deleteIllegalSteps(steps);
+    }
 
     public List<Position> findSolution()
     {
-        ArrayList<Position> solution = new ArrayList<Position>(2);
-        solution.add(field.getStart());
-        solution.add(field.getEnd());
+        System.out.println("End : " + field.getEnd());
+        System.out.println();
+        while (!currentPos.equals(field.getEnd()))
+        {
+            // I wanna lambda-functions!!!
+            List<Position> allSteps = getAllSteps();
+            Position next = allSteps.get(0);
+            Double min = Position.calcDistance(next, field.getEnd());
+            for (Position pos : allSteps)
+                if (min > Position.calcDistance(pos, field.getEnd()))
+                {
+                    next = pos;
+                    min = Position.calcDistance(pos, field.getEnd());
+                }
+            System.out.println("Next: " + next);
+            step(next);
+        }
+
         return solution;
+    }
+
+    Position getCurrentPos()
+    {
+        return currentPos;
     }
 }
--- ZD_Tech/src/test/java/ru/zdtech/robots/RobotTest.java
+++ ZD_Tech/src/test/java/ru/zdtech/robots/RobotTest.java
@@ -3,6 +3,8 @@ package ru.zdtech.robots;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Class of the project ZD_Tech
  * Developer: ivan
@@ -10,13 +12,73 @@ import org.junit.Test;
 public class RobotTest {
 
 
+    private Field simpleField5x5 = new Field(5, 5, new Position(0,0), new Position(0, 1));
+
     @Test
-    public void testFindSolution() throws Exception
+    public void testStep() throws Exception
     {
-        Field field = new Field(5, 5, new Position(0,0), new Position(0, 1));
+        Robot robot = new Robot(simpleField5x5);
+        Assert.assertEquals(robot.getCurrentPos(), simpleField5x5.getStart());
+        robot.step(new Position(0,1));
+        Assert.assertEquals(robot.getCurrentPos(), new Position(0,1));
+    }
+
+    @Test
+    public void testAllSteps() throws Exception
+    {
+        Robot robot = new Robot(simpleField5x5);
+        Assert.assertArrayEquals(robot.getAllSteps().toArray(),
+                                 new Position[] { new Position(1, 0), new Position(0, 1)});
+
+        robot = new Robot(new Field(10, 10, new Position(3,3), new Position(5,5)));
+        Assert.assertArrayEquals(new Position[] { new Position(3, 2), new Position(4, 3),
+                new Position(3, 4), new Position(2, 3)},
+                                 robot.getAllSteps().toArray());
+
+    }
+
+
+    @Test
+    public void testSimpleSolution() throws Exception
+    {
+        Robot robot = new Robot(simpleField5x5);
+        List<Position> solution = robot.findSolution();
+        Assert.assertArrayEquals(solution.toString(), solution.toArray(),
+                                 new Position[]{simpleField5x5.getStart(), simpleField5x5.getEnd()});
+    }
+
+    @Test
+    public void testLineSolution() throws Exception
+    {
+        Field field = new Field(5, 5, new Position(0, 0), new Position(0, 2));
         Robot robot = new Robot(field);
-        Assert.assertArrayEquals(robot.findSolution().toArray(),
-                                 new Position[]{ field.getStart(), field.getEnd() });
+        List<Position> solution = robot.findSolution();
+        Assert.assertArrayEquals(solution.toString(), solution.toArray(),
+                                 new Position[]{field.getStart(), new Position(0, 1), field.getEnd()});
+
+    }
+
+    @Test
+    public void testDioganalSolution() throws Exception
+    {
+        Field field = new Field(5, 5, new Position(0, 0), new Position(1, 1));
+        Robot robot = new Robot(field);
+        List<Position> solution = robot.findSolution();
+        Assert.assertArrayEquals(solution.toString(), solution.toArray(),
+                                 new Position[]{field.getStart(), new Position(1, 0), field.getEnd()});
+
+    }
+
+    @Test
+    public void testLongDioganalSolution() throws Exception
+    {
+        Field field = new Field(5, 5, new Position(0, 0), new Position(4, 2));
+        Robot robot = new Robot(field);
+        List<Position> solution = robot.findSolution();
+        Assert.assertArrayEquals(solution.toString(), solution.toArray(),
+                                 new Position[]{field.getStart(), new Position(1, 0), new Position(2, 0),
+                                                 new Position(3, 0), new Position(3, 1), new Position(4,1),
+                                                 field.getEnd()});
 
     }
 }
